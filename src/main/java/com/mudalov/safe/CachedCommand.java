4ba39aadce2f677b205d4e6d5fcb44d1a1a6697d
Mikhail Udalov <mikhail.udalov@gmail.com>
2015-01-25 14:18:48 +0300

    Docs for Cached Command

--- src/main/java/com/mudalov/safe/CachedCommand.java
+++ src/main/java/com/mudalov/safe/CachedCommand.java
@@ -1,6 +1,14 @@
 package com.mudalov.safe;
 
 
+/**
+ * If command implements this interface, then result will be cached (and constantly updated)
+ * and used as a fallback for further calls. Custom cache implementation can be configured for each Dependency Group.
+ *
+ * @see com.mudalov.safe.cache.Cache
+ * @see com.mudalov.safe.cache.CacheFactory
+ *
+ */
 public interface CachedCommand<T> extends Command<T> {
 
     public String getKey();
